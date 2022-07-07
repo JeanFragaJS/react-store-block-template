efd8b3d51515ee1a2ccf4b8e91e9aabc07219644
Jean Fraga <jeandevjs@gmail.com>
2022-07-06 21:09:24 -0300

    feat: learn about custom blocks by vtexio

--- react/Countdown.tsx
+++ react/Countdown.tsx
@@ -1,16 +1,84 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { TimeSplit } from './typings/global'
+import { tick, getTwoDaysFromNow } from './utils/time'
+import { useCssHandles } from 'vtex.css-handles'
 
-interface CountdownProps {}
+import { useQuery } from 'react-apollo'
+import useProduct from 'vtex.product-context/useProduct'
+import productReleaseDate from './queries/productReleaseDate.graphql' 
 
-const Countdown: StorefrontFunctionComponent<CountdownProps> = ({}) => {
-  return <div />
+interface CountdownProps {
+  targetDate: string
+}
+
+const CSS_HANDLES = ['countdown']
+const DEFAULT_TARGET_DATE = getTwoDaysFromNow()
+
+
+const Countdown: StorefrontFunctionComponent<CountdownProps> = () => {
+  const [timeRemaining, setTime] = useState<TimeSplit>({
+    hours: "00",
+    minutes: "00",
+    seconds: "00"
+  })
+
+  const handles  = useCssHandles(CSS_HANDLES)
+  
+  const { product } = useProduct()
+  const { data, loading, error } =  useQuery(productReleaseDate, {
+    variables: {
+      slug: product?.linkText
+    },
+    ssr: false
+  }) 
+  
+  console.log(data)
+  
+  if (!product) {
+    return (
+      <div>
+        <span>There is no product context.</span>
+      </div>
+    )
+  }
+  
+  if (loading) {
+    return (
+      <div>
+        <span>Loading...</span>
+      </div>
+    )
+  }
+  if (error) {
+    return (
+      <div>
+        <span>Erro!</span>
+      </div>
+    )
+  }
+  
+  tick(data?.product?.releaseDate || DEFAULT_TARGET_DATE, setTime)
+  
+  return (
+
+      <div className={`${handles.countdown} t-heading-2 c-muted-1 db tc`}>
+        {`${timeRemaining.hours}:${timeRemaining.minutes}:${timeRemaining.seconds}`}
+      </div>
+  )
 }
 
 Countdown.schema = {
   title: 'editor.countdown.title',
   description: 'editor.countdown.description',
   type: 'object',
-  properties: {},
+  properties: {
+    targetDate: {
+      title: 'editor.countdown.targetDate.title',
+      description: 'editor.countdown.targetDate.description',
+      type: "string",
+      default: null
+    }
+  },
 }
 
 export default Countdown
